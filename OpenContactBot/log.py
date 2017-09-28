f1edaacdf711a32aedd5311c1e91b86d426d3a79
partizanes <partizanes1@gmail.com>
2017-09-28 17:18:35 +0300

    [76] ignore UnicodeEncodeError in log.

--- OpenContactBot/log.py
+++ OpenContactBot/log.py
@@ -9,25 +9,43 @@ init()
 
 class Log(object):
     def __init__(self, Module=None):
-        self.Module = Module
-        logging.basicConfig(format = u'[%(asctime)s][%(levelname)s][' + self.Module + '] %(message)s', level = logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S', filename = u'bot.log')
+        try:
+            self.Module = Module
+            logging.basicConfig(format = u'[%(asctime)s][%(levelname)s][' + self.Module + '] %(message)s', level = logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S', filename = u'bot.log')
+        except UnicodeEncodeError:
+            pass
 
     def debug(self, message):
-        logging.debug( u'%s'%(message))
-        print(Fore.WHITE + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'DEBUG', message))
+        try:
+            logging.debug( u'%s'%(message))
+            print(Fore.WHITE + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'DEBUG', message))
+        except UnicodeEncodeError:
+            pass
 
     def info(self, message):
-        logging.info( u'%s'%(message))
-        print(Fore.GREEN + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'INFO', message))
+        try:
+            logging.info( u'%s'%(message))
+            print(Fore.GREEN + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'INFO', message))
+        except UnicodeEncodeError:
+            pass
 
     def warning(self, message):
-        logging.warning( u'%s'%(message))
-        print(Fore.LIGHTYELLOW_EX +  u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'WARN', message))
+        try:
+            logging.warning( u'%s'%(message))
+            print(Fore.LIGHTYELLOW_EX +  u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'WARN', message))
+        except UnicodeEncodeError:
+            pass
 
     def error(self, message):
-        logging.error( u'%s'%(message))
-        print(Fore.RED + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'ERROR', message))
+        try:
+            logging.error( u'%s'%(message))
+            print(Fore.RED + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'ERROR', message))
+        except UnicodeEncodeError:
+            pass
 
     def critical(self, message):
-        logging.error( u'%s'%(message))
-        print(Fore.LIGHTRED_EX + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'CRITICAL', message))
+        try:
+            logging.error( u'%s'%(message))
+            print(Fore.LIGHTRED_EX + u'[%s][%s][%s] %s'%(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), self.Module, 'CRITICAL', message))
+        except UnicodeEncodeError:
+            pass 
