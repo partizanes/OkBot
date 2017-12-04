c84c00ec0062027dc5dc1d99bd385351bcb49bf6
partizanes <partizanes1@gmail.com>
2017-12-04 17:36:10 +0300

    [139] add ipmiServerHandler class.

---
+++ OpenContactBot/ipmiServerHandler.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# by Part!zanes 2017
+
+from log import Log
+from time import sleep
+from cpanelapiclient import cpanelApiClient
+
+class ipmiServerHandler(object):
+    """description of class"""
+
+
